7e915677a8ddd78bf51fbcc03499d758dc735234
justin1732 <57789046+justin1732@users.noreply.github.com>
2020-03-27 18:12:54 -0500

    debugging

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.set("view engine", "handlebars");
 // Routes
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
-require("./routes/apilogin")(app);
+require("./routes/apiLogin")(app);
 
 var syncOptions = { force: false };
 
