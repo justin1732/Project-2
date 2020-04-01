fb4635cd565ba657f53b47d4aa97990ce9d19ec9
justin1732 <57789046+justin1732@users.noreply.github.com>
2020-04-01 17:47:49 -0500

    Pushing it back into the right area.

--- server.js
+++ server.js
@@ -114,7 +114,7 @@ require("./routes/apifavorites")(app);
 require("./routes/htmlRoutes")(app);
 
 
-var syncOptions = { force: true };
+var syncOptions = { force: false };
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
